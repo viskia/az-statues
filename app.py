d53cb93a26c6eac7596aacc64c5bb30eeccf18e3
violethenion <violethenion@gmail.com>
2026-09-24 09:08:21 -0700

    gcloudrunver

--- app.py
+++ app.py
@@ -432,5 +432,5 @@ def home():
     return page
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 10000))
-     app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
